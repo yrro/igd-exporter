23a47f4ac010893141515b129957cdc71ac7d1a7
Sam Morris <sam@robots.org.uk>
2016-10-30 01:51:32 +0000

    Add socket niceties

--- exporter.py
+++ exporter.py
@@ -47,10 +47,17 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--bind-address', type=ipaddress.ip_address, default='::', help='IPv6 or IPv4 address to listen on')
     parser.add_argument('--bind-port', type=int, default=9196, help='Port to listen on')
+    parser.add_argument('--bind-v6only', type=int, choices=[0, 1], help='If 1, prevent IPv6 sockets from accepting IPv4 connections; if 0, allow; if unspecified, use OS default')
     args = parser.parse_args()
 
     class IPv6Server(wsgiref.simple_server.WSGIServer):
         address_family = socket.AF_INET6 if args.bind_address.version == 6 else socket.AF_INET
+
+        def server_bind(self):
+            self.socket.setsockopt(socket.IPPROTO_IP, 15, 1) # IP_FREEBIND
+            if args.bind_v6only is not None and args.bind_address.version == 6:
+                self.socket.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, args.bind_v6only)
+            super().server_bind()
 
     s = wsgiref.simple_server.make_server(str(args.bind_address), args.bind_port, wsgi_app, IPv6Server)
     s.serve_forever()
