0dc3f20842b693c02e741e6334827844e9402609
Sam Morris <sam@robots.org.uk>
2016-10-31 01:49:16 +0000

    Replace targets list rather than modifying it during device search
    Avoids problems from multiple concurrent calls to the front and search
    apps, which read and modify the list respectively.

--- exporter.py
+++ exporter.py
@@ -43,12 +43,14 @@ def front(environ, start_response):
     Front page, containing links to the expoter's own metrics, as well as links
     to probe discovered devices.
     '''
+    global targets
+
     start_response('200 OK', [('Content-Type', 'text/html')])
 
     if environ['REQUEST_METHOD'] == 'POST':
         form = cgi.FieldStorage(fp=environ['wsgi.input'], environ=environ, strict_parsing=1, encoding='latin1')
         if form.getfirst('search') == '1':
-            targets[:] = igd.search(5)
+            targets = igd.search(5)
 
     return [
         b'<html>'
